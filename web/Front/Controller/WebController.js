e07b7ec0506a6919b850e2150c5c1d0a9f0da9bd
berto <albertomartinnot@gmail.com>
2017-05-04 16:58:14 +0200

    Backend first steps,databases and modal creation

--- web/Front/Controller/WebController.js
+++ web/Front/Controller/WebController.js
@@ -1,5 +1,8 @@
 GamesWeb.controller('ctr1',function($scope,$http){
+	$scope.addUser=function($usuario){
+		$http.post('http://localhost:8000/adduser',$usuario).then(location.replace('http://localhost:8000/Front/articulos'));
 
+	}
 
 
 });
--- web/Front/config.js
+++ web/Front/config.js
@@ -3,7 +3,11 @@ var GamesWeb=angular.module('GamesWeb', ['ngRoute']);
 GamesWeb.config(function($routeProvider){
 	$routeProvider.when('/', {
 		templateUrl: 'Templates/info.template.html',
-		controller: 'Controller/WebController.js'
+		controller: 'ctr1'
+	})
+	.when('/articulos', {
+		templateUrl: 'Templates/articulos.template.html',
+		controller: 'ctr1'
 	})
 
 });
