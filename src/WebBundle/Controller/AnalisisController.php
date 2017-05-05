81e7d47ad571e6be10aee1ad6cecf441e674b308
berto <albertomartinnot@gmail.com>
2017-05-05 16:56:42 +0200

    Backend development first steps

---
+++ src/WebBundle/Controller/AnalisisController.php
@@ -0,0 +1,35 @@
+<?php  
+namespace WebBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as REST;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+use WebBundle\Entity\user;
+use WebBundle\Entity\analisis;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+
+class WebController extends FOSRestController{
+
+
+
+
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+?>
